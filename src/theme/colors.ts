580617cf1f052cc744e3b6751bd4ae09967abef0
Victor Aponte <vaponte520@gmail.com>
2025-04-21 14:53:02 -0300

    Agregar archivo de colores con la definición de la paleta de colores del tema

---
+++ src/theme/colors.ts
@@ -0,0 +1,16 @@
+const colors = {
+    primary: '#4488b3',
+    secondary: '#FFC107',
+    background: '#FFFFFF',
+    text: '#212121',
+    textSecondary: '#757575',
+    error: '#f44336',
+    success: '#4caf50',
+    grayLight: '#E0E0E0',
+    grayDark: '#9E9E9E',
+    black: '#000000',
+    white: '#FFFFFF',
+  };
+  
+  export default colors;
+  
