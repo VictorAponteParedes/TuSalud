d1e74cf57d3d62f01e4f1d6f9c242f3e94e9c7bf
Victor Aponte <vaponte520@gmail.com>
2025-04-21 14:52:49 -0300

    Reemplazar TextInput por componente Input en el formulario de inicio de sesión

--- src/screens/Auth/Login/LoginScreen.tsx
+++ src/screens/Auth/Login/LoginScreen.tsx
@@ -1,14 +1,29 @@
 
 import React from 'react';
-import { View, TextInput, Button } from 'react-native';
+import {View, TextInput} from 'react-native';
+import Button from '../../../components/Ui/Button';
+import Input from '../../../components/Ui/Input';
+
 import styles from './styles';
 
 const LoginForm = () => {
   return (
     <View style={styles.form}>
-      <TextInput style={styles.input} placeholder="Correo electrónico" />
-      <TextInput style={styles.input} placeholder="Contraseña" secureTextEntry />
-      <Button title="Ingresar" onPress={() => console.log('Login pressed')} />
+      <Input
+        label="Nombre de usuario"
+        placeholder="Ingresa tu nombre de usuario"
+      />
+      <Input
+        label="Contraseña"
+        placeholder="Ingresa tu contraseña"
+        secureTextEntry={true}
+      />
+      <Button
+        title="Iniciar sesión"
+        onPress={() => console.log('Botón presionado')}
+        loading={false}
+        disabled={false}
+      />
     </View>
   );
 };
