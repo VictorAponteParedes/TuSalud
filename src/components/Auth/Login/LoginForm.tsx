9c62e68ec3fa68f433a1585e0c18f82aedad8554
Victor Aponte <vaponte520@gmail.com>
2025-04-22 14:13:50 -0300

    Refactor el formulario de inicio de sesión para utilizar el componente Input y eliminar la lógica de formulario

--- src/components/Auth/Login/LoginForm.tsx
+++ src/components/Auth/Login/LoginForm.tsx
@@ -1,62 +1,30 @@
-// src/components/Auth/LoginForm.tsx
-
 import React from 'react';
-import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
-import { useForm, Controller } from 'react-hook-form';
+import {View, TextInput} from 'react-native';
+import Button from '../../../components/Ui/Button';
+import Input from '../../Ui/Input';
+
 import styles from './styles';
 
-type LoginFormData = {
-  email: string;
-  password: string;
-};
-
 const LoginForm = () => {
-  const { control, handleSubmit, formState: { errors } } = useForm<LoginFormData>();
-
-  const onSubmit = (data: LoginFormData) => {
-    console.log('Login data:', data);
-  };
-
   return (
     <View style={styles.form}>
-      <Controller
-        control={control}
-        name="email"
-        rules={{ required: 'Correo es requerido' }}
-        render={({ field: { onChange, value } }) => (
-          <TextInput
-            style={styles.input}
-            placeholder="Correo electrónico"
-            value={value}
-            onChangeText={onChange}
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
-        )}
+      <Input
+        label="Nombre de usuario"
+        placeholder="Ingresa tu nombre de usuario"
       />
-      {errors.email && <Text style={styles.error}>{errors.email.message}</Text>}
-
-      <Controller
-        control={control}
-        name="password"
-        rules={{ required: 'Contraseña es requerida' }}
-        render={({ field: { onChange, value } }) => (
-          <TextInput
-            style={styles.input}
-            placeholder="Contraseña"
-            secureTextEntry
-            value={value}
-            onChangeText={onChange}
-          />
-        )}
+      <Input
+        label="Contraseña"
+        placeholder="Ingresa tu contraseña"
+        secureTextEntry={true}
       />
-      {errors.password && <Text style={styles.error}>{errors.password.message}</Text>}
-
-      <Button title="Ingresar" onPress={handleSubmit(onSubmit)} />
+      <Button
+        title="Iniciar sesión"
+        onPress={() => console.log('Botón presionado')}
+        loading={false}
+        disabled={false}
+      />
     </View>
   );
 };
 
 export default LoginForm;
-
-
