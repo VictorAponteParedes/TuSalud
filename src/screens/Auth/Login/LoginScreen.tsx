a8d566f9508830391db809204ad5d0a11ad5782a
Victor Aponte <vaponte520@gmail.com>
2025-04-26 21:11:05 -0300

    Translate welcome messages in LoginScreen for multilingual support

--- src/screens/Auth/Login/LoginScreen.tsx
+++ src/screens/Auth/Login/LoginScreen.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import { View, Text, Image } from 'react-native';
 import LoginForm from '../../../components/Auth/Login/LoginForm';
-import {logoInicial} from '../../../assets';
+import { logoInicial } from '../../../assets';
 import styles from './styles';
+import { translate } from '../../../lang';
 
 const LoginScreen = () => {
   return (
     <View style={styles.container}>
       <Image source={logoInicial} style={styles.logo} />
-      <Text style={styles.title}>Bienvenido de nuevo!</Text>
-      <Text style={styles.subTitle}>Inicia sesión para continuar</Text>
+      <Text style={styles.title}>{translate("WelcomeBack")}</Text>
+      <Text style={styles.subTitle}>{translate("Login")}</Text>
       <View style={styles.modal}>
         <LoginForm />
       </View>
