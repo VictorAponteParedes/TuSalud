33bd6ae3053bbc1fd4a27f7052c9c9eedc5a9793
Victor Aponte <vaponte520@gmail.com>
2025-04-26 21:09:57 -0300

    Reorganizar el código de estilos en el componente de inicio de sesión para mejorar la legibilidad

--- src/screens/Auth/Login/styles.tsx
+++ src/screens/Auth/Login/styles.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet } from "react-native";
 import colors from '../../../theme/colors';
-import {Dimensions} from 'react-native';
-const {height} = Dimensions.get('window');
+import { Dimensions } from 'react-native';
+const { height } = Dimensions.get('window');
 
 
 const styles = StyleSheet.create({
@@ -41,10 +41,12 @@ const styles = StyleSheet.create({
     padding: 20,
     elevation: 10,
     shadowColor: '#000',
-    shadowOffset: {width: 0, height: -2},
+    shadowOffset: { width: 0, height: -2 },
     shadowOpacity: 0.1,
     shadowRadius: 8,
     justifyContent: 'center',
   },
+
+
 });
 export default styles;  
