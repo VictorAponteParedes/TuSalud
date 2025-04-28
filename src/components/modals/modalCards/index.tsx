0fb08c527a4a49a31c541329861ba3a8afc81fd4
Victor Aponte <vaponte520@gmail.com>
2025-04-28 18:21:50 -0300

    Agregar componente ModalCards: implementar estructura y estilos para mostrar imágenes y texto

---
+++ src/components/modals/modalCards/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+import styles from './styles';
+import { ModalCardsProps } from "../../../types/modals";
+
+const ModalCards = (props: ModalCardsProps) => {
+  const { imagenItem, title, subTitile } = props;
+
+  return (
+    <View style={styles.card}>
+      <Image
+        source={
+          typeof imagenItem === 'string'
+            ? { uri: imagenItem }
+            : imagenItem
+        }
+        style={styles.image}
+      />
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.subtitle}>{subTitile}</Text>
+    </View>
+  );
+};
+
+export default ModalCards;
