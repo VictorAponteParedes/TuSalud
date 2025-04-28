1861bd885c4bc4c76c633101e9b3cc9b6f07cc08
Victor Aponte <vaponte520@gmail.com>
2025-04-28 18:22:28 -0300

    Refactor HomeScreen: mejorar estructura y estilos, agregar ModalCards para mostrar servicios

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,21 +1,50 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-import { useAuth } from '../../context/AuthContext';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {useAuth} from '../../context/AuthContext';
 import CustomHeader from '../../components/customHeader';
-import {profileImage} from '../../assets';
+import {profileImage, itemMedicine} from '../../assets';
+import ModalCards from '../../components/modals/modalCards';
+import styles from './styles';
+import {translate} from '../../lang';
 
 const HomeScreen = () => {
   const {logout} = useAuth();
+
   return (
     <>
       <CustomHeader imageProfile={profileImage} title="Home" />
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{color: 'red'}}>Home Screen</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>{translate('newServices')}</Text>
+
+        <View style={styles.cardsContainer}>
+          <ModalCards
+            imagenItem={itemMedicine}
+            title={translate('Citas')}
+            subTitile={translate('appontment')}
+          />
+          <ModalCards
+            imagenItem={itemMedicine}
+            title={translate('Citas')}
+            subTitile={translate('appontment')}
+          />
+          <ModalCards
+            imagenItem={itemMedicine}
+            title={translate('Citas')}
+            subTitile={translate('appontment')}
+          />
+          <ModalCards
+            imagenItem={itemMedicine}
+            title={translate('Citas')}
+            subTitile={translate('appontment')}
+          />
+        </View>
+
         <TouchableOpacity onPress={logout}>
-          <Text>Logout</Text>
+          <Text style={styles.logoutText}>Logout</Text>
         </TouchableOpacity>
       </View>
     </>
   );
 };
+
 export default HomeScreen;
