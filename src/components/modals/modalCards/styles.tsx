29eae8ed6d8964a18993dda060e5f427ecfa67a7
Victor Aponte <vaponte520@gmail.com>
2025-04-28 18:21:55 -0300

    Agregar archivo de estilos para el componente ModalCards: definir estilos para card, image, title y subtitle

---
+++ src/components/modals/modalCards/styles.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    card: {
+      width: '48%',
+      backgroundColor: '#fff',
+      borderRadius: 12,
+      padding: 16,
+      marginBottom: 16,
+      shadowColor: '#000',
+      shadowOpacity: 0.1,
+      shadowOffset: { width: 0, height: 2 },
+      shadowRadius: 6,
+      elevation: 3,
+    },
+    image: {
+      width: 60,
+      height: 60,
+      marginBottom: 10,
+      borderRadius: 30,
+    },
+    title: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      marginBottom: 6,
+      textAlign: 'center',
+    },
+    subtitle: {
+      fontSize: 14,
+      color: '#666',
+      textAlign: 'center',
+    },
+  });
+
+  export default styles;
