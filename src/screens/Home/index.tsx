70ec7f0b4e3cd3a67d59198e71efb59ae1f82aee
Victor Aponte <vaponte520@gmail.com>
2025-04-28 15:26:18 -0300

    Refactor HomeScreen y ProfileScreen: agregar CustomHeader y mejorar la estructura del componente

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { useAuth } from '../../context/AuthContext';
+import CustomHeader from '../../components/customHeader';
+import {profileImage} from '../../assets';
 
 const HomeScreen = () => {
-  const { logout } = useAuth();
+  const {logout} = useAuth();
   return (
-    <View>
-      <Text>Home Screen</Text>
-
-      <TouchableOpacity onPress={logout}>
-        <Text style={{ color: "black" }}>Logout</Text>
-      </TouchableOpacity>
-    </View>
+    <>
+      <CustomHeader imageProfile={profileImage} title="Home" />
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{color: 'red'}}>Home Screen</Text>
+        <TouchableOpacity onPress={logout}>
+          <Text>Logout</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 };
 export default HomeScreen;
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { View, Text } from "react-native";
-
+import CustomHeader from '../../components/customHeader';
+import {translate} from '../../lang';
+import {useNavigation} from '@react-navigation/native';
 const ProfileScreen = () => {
+  const navigation = useNavigation();
   return (
-    <View>
+    <>
+      <CustomHeader
+        titleBack={translate('backToLogin')}
+        onBackPress={navigation.goBack}
+      />
       <Text>Profile Screen</Text>
-    </View>
+    </>
   );
 };
 export default ProfileScreen;
