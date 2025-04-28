73cae825bc05483948024899141bfb5cedbcd4b9
Victor Aponte <vaponte520@gmail.com>
2025-04-28 18:21:43 -0300

    Agregar imagen de medicina y actualizar exportaciones en assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,5 @@
 import logoInicial from './images/logoInicial.png';
 import profileImage from './images/profile2.jpeg';
+import itemMedicine from './images/itemMedicine.png';
 
-export { logoInicial, profileImage };
+export { logoInicial, profileImage, itemMedicine };
