edadd4307e72fe429b3da372d57c54a85957c0b6
Victor Aponte <vaponte520@gmail.com>
2025-04-29 14:01:27 -0300

    refactor: mejorar la estructura del componente HomeScreen con ScrollView

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView} from 'react-native';
 import CustomHeader from '../../components/customHeader';
 import {profileImage} from '../../assets';
 import ModalCards from '../../components/modals/modalCards';
@@ -10,22 +10,24 @@ import {informationHome} from '../../mock/modalCard';
 const HomeScreen = () => {
   return (
     <>
-      <CustomHeader imageProfile={profileImage} title="Home" />
-      <View style={styles.container}>
-        <Text style={styles.title}>{translate('newServices')}</Text>
+      <CustomHeader imageProfile={profileImage} title="Hola, Victor Aponte" />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.container}>
+          <Text style={styles.title}>{translate('newServices')}</Text>
 
-        <View style={styles.cardsContainer}>
-          {informationHome.map((item, index) => (
-            <ModalCards
-              key={index}
-              imagenItem={item.image}
-              title={item.title}
-              subTitile={item.description}
-            />
-          ))}
-          <Text style={styles.title}>{translate('informationCovid')}</Text>
+          <View style={styles.cardsContainer}>
+            {informationHome.map((item, index) => (
+              <ModalCards
+                key={index}
+                imagenItem={item.image}
+                title={item.title}
+                subTitile={item.description}
+              />
+            ))}
+            <Text style={styles.title}>{translate('informationCovid')}</Text>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </>
   );
 };
