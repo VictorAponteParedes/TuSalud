78dfb286a8bff994ee4405148dc02397bb7790ed
Victor Aponte <vaponte520@gmail.com>
2025-04-28 22:47:27 -0300

    Refactor CustomHeader component: clean up imports, remove unused interface, and improve code formatting

--- src/components/customHeader/index.tsx
+++ src/components/customHeader/index.tsx
@@ -1,21 +1,16 @@
 import React from "react";
-import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import { Text, TouchableOpacity, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import colors from '../../theme/colors';
-import {translate} from '../../lang';
+import { translate } from '../../lang';
 import Routes from '../../navigation/routes';
 import LinearGradient from 'react-native-linear-gradient';
+import styles from './styles';
+import { CustomHeaderProps } from "../../types/auth";
 
-interface CustomHeaderProps {
-  title?: string;
-  titleBack?: string;
-  onBackPress?: () => void;
-  imageProfile?: string;
-  gradientColors?: string[];
-}
 
 const CustomHeader = (props: CustomHeaderProps) => {
-  const {title, titleBack, onBackPress, imageProfile, gradientColors} = props;
+  const { title, titleBack, onBackPress, imageProfile, gradientColors } = props;
   const navigation = useNavigation();
 
   return (
@@ -41,10 +36,10 @@ const CustomHeader = (props: CustomHeaderProps) => {
           <Image
             source={
               typeof imageProfile === 'string'
-                ? {uri: imageProfile}
+                ? { uri: imageProfile }
                 : imageProfile
             }
-            style={{width: 40, height: 40, borderRadius: 20}}
+            style={{ width: 40, height: 40, borderRadius: 20 }}
           />
         </TouchableOpacity>
       )}
@@ -53,24 +48,3 @@ const CustomHeader = (props: CustomHeaderProps) => {
 };
 
 export default CustomHeader;
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: 16,
-    backgroundColor: colors.primary[50],
-    paddingVertical: 30,
-    borderStartEndRadius: 20,
-    borderBottomEndRadius: 20,
-  },
-  title: {
-    fontSize: 20,
-    color: colors.white,
-  },
-  backButton: {
-    fontSize: 16,
-    color: colors.white,
-  },
-});
