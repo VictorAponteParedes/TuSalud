d4b70536ccbd7688907c1205ae40f9b59341f87d
Victor Aponte <vaponte520@gmail.com>
2025-04-29 13:54:07 -0300

    feat: agregar estilos y tamaños de texto para la pantalla de perfil

---
+++ src/screens/Profile/styles.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+import colors from "../../theme/colors";
+
+
+const styles = StyleSheet.create({
+    logoutText: {
+        marginTop: 30,
+        fontSize: 18,
+        textAlign: 'center',
+        color: colors.error,
+      },
+});
+
+export default styles;
---
+++ src/theme/size.ts
@@ -0,0 +1,10 @@
+
+const sizeText = {
+    title:{
+        
+        title: 20,
+        subtitle: 16,
+        text: 14,
+    }
+}
+export default sizeText;
