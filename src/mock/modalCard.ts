56eb003ef779d0d8642095c3f782338b5a36deff
Victor Aponte <vaponte520@gmail.com>
2025-04-29 13:52:58 -0300

    feat: agregar archivo modalCard.ts con información para la pantalla de inicio

---
+++ src/mock/modalCard.ts
@@ -0,0 +1,27 @@
+import { translate } from "../lang"
+import { itemMedicine } from "../assets"
+
+export const informationHome = [
+    {
+        title: translate('citas'),
+        description: translate('appontment'),
+        image: itemMedicine,
+    },
+    {
+        title: translate('citas'),
+        description: translate('appontment'),
+        image: itemMedicine,
+    },
+    {
+        title: translate('citas'),
+        description: translate('appontment'),
+        image: itemMedicine,
+    },
+    {
+        title: translate('citas'),
+        description: translate('appontment'),
+        image: itemMedicine,
+    },
+    
+    
+]
