5b7b5e4c689b387528734d4aec84bdf59d2b5e8e
Victor Aponte <vaponte520@gmail.com>
2025-04-29 13:53:35 -0300

    refactor: ajustar el tamaño de fuente en el estilo del título en HomeScreen

--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -1,28 +1,29 @@
 
 import { StyleSheet } from 'react-native';
 import colors from '../../theme/colors';
+import sizeText from '../../theme/size';
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 20,
-    },
-    title: {
-      color: colors.black,
-      fontSize: 24,
-      marginBottom: 20,
-    },
-    cardsContainer: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'space-between',
-    },
-    logoutText: {
-      marginTop: 30,
-      fontSize: 18,
-      textAlign: 'center',
-      color: 'blue',
-    },
-  });
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    color: colors.black,
+    fontSize: sizeText.title.title,
+    marginBottom: 20,
+  },
+  cardsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  logoutText: {
+    marginTop: 30,
+    fontSize: 18,
+    textAlign: 'center',
+    color: 'blue',
+  },
+});
 
   export default styles;
