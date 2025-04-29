85924dd450941f3100d6367f102a19751a59a240
Victor Aponte <vaponte520@gmail.com>
2025-04-29 13:53:53 -0300

    feat: agregar funcionalidad de cierre de sesión en la pantalla de perfil

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,17 +1,26 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {View, Text, TouchableOpacity} from 'react-native';
 import CustomHeader from '../../components/customHeader';
 import {translate} from '../../lang';
 import {useNavigation} from '@react-navigation/native';
+import {useAuth} from '../../context/AuthContext';
+import styles from './styles';
+
 const ProfileScreen = () => {
   const navigation = useNavigation();
+  const {logout} = useAuth();
   return (
     <>
       <CustomHeader
         titleBack={translate('backToLogin')}
         onBackPress={navigation.goBack}
+        iconBack={true}
       />
       <Text>Profile Screen</Text>
+
+      <TouchableOpacity onPress={logout}>
+        <Text style={styles.logoutText}>{translate('logout')}</Text>
+      </TouchableOpacity>
     </>
   );
 };
