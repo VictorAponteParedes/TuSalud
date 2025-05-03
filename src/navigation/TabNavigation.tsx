2e0c49f4dce69f8ca90b2cb4d38fdf5c8d155082
Victor Aponte <vaponte520@gmail.com>
2025-05-03 15:42:09 -0300

    refactor: standardize icon imports and improve tab bar icon rendering in TabNavigator

--- src/navigation/TabNavigation.tsx
+++ src/navigation/TabNavigation.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Routes from './routes';
-import profileIcon from '../assets/svg/profile.svg';
-import homeIcon from '../assets/svg/home.svg';
-import quotesIcon from '../assets/svg/quotes.svg';
+import HomeIcon from "../assets/svg/home.svg";
+import ProfileIcon from "../assets/svg/profile.svg";
+import CalendarIcon from "../assets/svg/calendar.svg";
+import SettingIcon from "../assets/svg/settings.svg";
+import { translate } from '../lang';
+import SvgWrapper from "../components/SvgWrapper";
 import colors from '../theme/colors';
-import {translate} from '../lang';
-
-//Views
+// Views
 import HomeScreen from '../screens/Home';
+import SettingsScreen from '../screens/Settings';
 import ProfileScreen from '../screens/Profile';
 import Quotes from '../screens/Quotes';
 
@@ -21,29 +23,46 @@ const TabNavigator = () => {
         headerShown: false,
         tabBarActiveTintColor: colors.primary[400],
         tabBarInactiveTintColor: colors.grayDark,
+        tabBarStyle: {
+          paddingBottom: 5,
+          height: 60,
+        },
       }}>
       <Tab.Screen
         name={Routes.HOME}
         component={HomeScreen}
         options={{
-          tabBarIcon: homeIcon,
+          tabBarIcon: ({ focused, color, size }) => (
+            <SvgWrapper color={color} size={size}>
+              <HomeIcon />
+            </SvgWrapper>
+          ),
           tabBarLabel: translate('Home'),
-        }}
-      />
-      <Tab.Screen
-        name={Routes.PROFILE}
-        component={ProfileScreen}
-        options={{
-          tabBarIcon: profileIcon,
-          tabBarLabel: translate('Profile'),
         }}
       />
       <Tab.Screen
         name={Routes.QUOTES}
         component={Quotes}
         options={{
-          tabBarIcon: quotesIcon,
+          tabBarIcon: ({ focused, color, size }) => (
+            <SvgWrapper color={color} size={size}>
+              <CalendarIcon />
+            </SvgWrapper>
+          ),
           tabBarLabel: translate('Quotes'),
+        }}
+      />
+
+      <Tab.Screen
+        name={Routes.SETTINGS}
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <SvgWrapper color={color} size={size}>
+              <SettingIcon />
+            </SvgWrapper>
+          ),
+          tabBarLabel: translate('Settings'),
         }}
       />
     </Tab.Navigator>
