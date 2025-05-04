5708a5c4b9683cd3cb9c4e179e4824f2ae5877ab
Victor Aponte <vaponte520@gmail.com>
2025-05-03 22:13:05 -0300

    feat: add Notifications screen to MainNavigator and update route definitions

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -2,14 +2,14 @@
 
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '../screens/Home';
-import ProfileScreen from '../screens/Profile';
 import Routes from './routes';
 import TabNavigator from './TabNavigation';
+import ProfileScreen from '../screens/Profile';
+import Notifications from '../screens/Notifications';
 
 export type MainStackParamList = {
-  Home: undefined;
   Profile: undefined;
+  Notifications: undefined;
   TabNavigator: undefined;
 };
 
@@ -17,10 +17,24 @@ const Stack = createNativeStackNavigator<MainStackParamList>();
 
 const MainNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen
         name={Routes.TAB_NAVIGATOR}
         component={TabNavigator}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name={Routes.PROFILE}
+        component={ProfileScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name={Routes.NOTIFICATIONS}
+        component={Notifications}
         options={{
           headerShown: false,
         }}
