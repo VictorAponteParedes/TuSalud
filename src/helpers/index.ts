bf327cdad51a3141f818fd81933b031898dac4d2
Victor Aponte <vaponte520@gmail.com>
2025-05-06 22:03:35 -0300

    feat: add helper module for SVG icon imports

---
+++ src/helpers/index.ts
@@ -0,0 +1,16 @@
+import Home from "../assets/svg/home.svg";
+import Profile from "../assets/svg/profile.svg";
+import Calendar from "../assets/svg/calendar.svg";
+import Setting from "../assets/svg/settings.svg";
+import Logout from "../assets/svg/logout.svg";
+import Notifications from "../assets/svg/notifications.svg";
+
+
+export {
+    Home,
+    Profile,
+    Calendar,
+    Setting,
+    Logout,
+    Notifications
+}
