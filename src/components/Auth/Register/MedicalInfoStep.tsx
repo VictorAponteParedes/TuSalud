dc26cd90e0164560595b65e57c92eb9c41484813
Victor Aponte <vaponte520@gmail.com>
2025-05-14 18:57:11 -0300

    fix: correct input field names in MedicalInfoStep component

--- src/components/Auth/Register/MedicalInfoStep.tsx
+++ src/components/Auth/Register/MedicalInfoStep.tsx
@@ -9,12 +9,27 @@ const MedicalInfoStep = ({ control }: { control: any }) => (
             label={translate('medicalInformation.birthDateLabel')}
             placeholder={translate('medicalInformation.birthDateInput')}
             control={control}
-            name="dataBorth"
+            name="dateBirth"
         />
 
-        <Input label={translate('medicalInformation.bloodTypeLabel')} placeholder={translate('medicalInformation.bloodTypeInput')} control={control} name="typeBloot" />
-        <Input label={translate('medicalInformation.allergiesLabel')} placeholder={translate('medicalInformation.bloodTypeInput')} control={control} name="alergias" />
-        <Input label={translate('medicalInformation.emergencyContactLabel')} placeholder={translate('medicalInformation.emergencyContactInput')} control={control} name="contactEmergecy" />
+        <Input
+            label={translate('medicalInformation.bloodTypeLabel')}
+            placeholder={translate('medicalInformation.bloodTypeInput')}
+            control={control}
+            name="bloodType" />
+
+        <Input
+            label={translate('medicalInformation.allergiesLabel')}
+            placeholder={translate('medicalInformation.bloodTypeInput')}
+            control={control}
+            name="allergies" />
+
+        <Input
+            label={translate('medicalInformation.emergencyContactLabel')}
+            placeholder={translate('medicalInformation.emergencyContactInput')}
+            control={control}
+            name="contactEmergency" />
+
     </>
 );
 
