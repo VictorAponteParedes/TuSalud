06f32fdfdd44b49482eb0dabbd76140ea3cc7526
Victor Aponte <vaponte520@gmail.com>
2025-05-16 22:35:19 -0300

    feat: enhance PersonalInfoStep to include profile image selection functionality

--- src/components/Auth/Register/PersonalInfoStep.tsx
+++ src/components/Auth/Register/PersonalInfoStep.tsx
@@ -1,43 +1,68 @@
 // src/components/Auth/RegisterSteps/PersonalInfoStep.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../../ui/Input';
+import InputImage from '../../ui/InputImage';
 import { translate } from '../../../lang';
+import { Control, UseFormSetValue } from 'react-hook-form';
+import { RegisterFormData } from '../../../types/auth';
 
-const PersonalInfoStep = ({ control }: { control: any }) => (
-    <>
-        <Input
-            label={translate('personalInfo.name')}
-            placeholder={translate('personalInfo.insertName')}
-            control={control}
-            name="firstName"
-        />
-        <Input
-            label={translate('personalInfo.lastName')}
-            placeholder={translate('personalInfo.insertLastName')}
-            control={control}
-            name="lastName"
-        />
-        <Input
-            label={translate('personalInfo.phone')}
-            placeholder={translate('personalInfo.insertPhone')}
-            control={control}
-            name="phone"
-        />
-        <Input
-            label={translate('personalInfo.address')}
-            placeholder={translate('personalInfo.insertAddress')}
-            control={control}
-            name="address"
-        />
-        <Input
-            label={translate('personalInfo.email')}
-            placeholder={translate('personalInfo.insertEmail')}
-            control={control}
-            name="email"
-            keyboardType="email-address"
-            autoCapitalize="none"
-        />
-    </>
-);
+const PersonalInfoStep = ({
+    control,
+    setValue,
+}: {
+    control: Control<RegisterFormData>;
+    setValue: UseFormSetValue<RegisterFormData>;
+}) => {
+
+
+    const [imageUri, setImageUri] = useState<string | undefined>();
+
+    const onImageSelected = (image: any) => {
+        setImageUri(image.uri);
+        setValue('profileImage', image);
+    };
+
+    return (
+        <>
+            <Input
+                label={translate('personalInfo.name')}
+                placeholder={translate('personalInfo.insertName')}
+                control={control}
+                name="firstName"
+            />
+            <Input
+                label={translate('personalInfo.lastName')}
+                placeholder={translate('personalInfo.insertLastName')}
+                control={control}
+                name="lastName"
+            />
+            <Input
+                label={translate('personalInfo.phone')}
+                placeholder={translate('personalInfo.insertPhone')}
+                control={control}
+                name="phone"
+            />
+            <Input
+                label={translate('personalInfo.address')}
+                placeholder={translate('personalInfo.insertAddress')}
+                control={control}
+                name="address"
+            />
+            <Input
+                label={translate('personalInfo.email')}
+                placeholder={translate('personalInfo.insertEmail')}
+                control={control}
+                name="email"
+                keyboardType="email-address"
+                autoCapitalize="none"
+            />
+            <InputImage
+                label="Foto de perfil"
+                onImageSelected={onImageSelected}
+                imageUri={imageUri}
+            />
+        </>
+    )
+};
 
 export default PersonalInfoStep;
