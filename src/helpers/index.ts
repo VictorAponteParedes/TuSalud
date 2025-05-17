02e9fa944a15ddf7cfa6eaf304651ad54ced318f
Victor Aponte <vaponte520@gmail.com>
2025-05-17 15:39:09 -0300

    feat: add fixUrl helper function to replace localhost with LOCAL_IP

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -16,12 +16,19 @@ import Drop from "../assets/svg/drop.svg"
 import Phone from "../assets/svg/phone.svg"
 import Email from "../assets/svg/email.svg"
 
+
+import { LOCAL_IP } from "../constants";
+
 export const groupArray = (array: [], groupSize: number) => {
     const groups = [];
     for (let i = 0; i < array.length; i += groupSize) {
         groups.push(array.slice(i, i + groupSize));
     }
     return groups;
+};
+
+export const fixUrl = (url: string) => {
+    return url.replace('localhost', LOCAL_IP);
 };
 
 
