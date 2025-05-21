bdbc0a3e4b3c09d686eeb259c7f9c7cb84988d9f
Victor Aponte <vaponte520@gmail.com>
2025-05-21 14:53:53 -0300

    refactor: actualizar comentarios y mejorar la estructura de las constantes de URL

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,6 @@
-export const LOCAL_IP = '192.168.0.106';
-export const API_BASE_URL = `http://${LOCAL_IP}:3000`
+//Para levantar en la windonws
+// export const LOCAL_IP = '192.168.0.106';
+// export const API_BASE_URL = `http://${LOCAL_IP}:3000`
+
+//Para levantar en la mac
+export const API_BASE_URL = 'http://10.0.2.2:3000';
