dfcb0bc9f298e283d31869b55faa95c430c03ff3
Victor Aponte <vaponte520@gmail.com>
2025-05-21 12:51:09 -0300

    feat: agregar funcionalidad de restablecimiento de contraseña y mejorar la navegación en AuthNavigator

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/Auth/Login/LoginScreen';
 import RegisterScreen from '../screens/Auth/Register/RegisterScreen';
 import ForgotPasswordScreen from '../screens/Auth/ForgotPassword';
+import ResetPasswordScreen from '../screens/Auth/ResetPassword';
 import Routes from './routes';
 
 export type AuthStackParamList = {
@@ -16,10 +17,17 @@ const Stack = createNativeStackNavigator<AuthStackParamList>();
 
 const AuthNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name={Routes.LOGIN} component={LoginScreen} />
       <Stack.Screen name={Routes.REGISTER} component={RegisterScreen} />
-      <Stack.Screen name={Routes.FORGOT_PASSWORD} component={ForgotPasswordScreen} />
+      <Stack.Screen
+        name={Routes.FORGOT_PASSWORD}
+        component={ForgotPasswordScreen}
+      />
+      <Stack.Screen
+        name={Routes.RESET_PASSWORD}
+        component={ResetPasswordScreen}
+      />
     </Stack.Navigator>
   );
 };
