d1e230cbcdce196d0e0d9d69f1ab201a22e5e277
Victor Aponte <vaponte520@gmail.com>
2025-06-16 18:43:17 -0300

    fix(status): enhance status styles with text labels and update yellow color palette

--- src/helpers/status.ts
+++ src/helpers/status.ts
@@ -1,15 +1,32 @@
+// helpers/status.ts
 import colors from "../theme/colors";
 import { StatusAppointment } from "../enum/statusAppointment";
 
 export const getStatusStyle = (status: string) => {
     switch (status) {
         case StatusAppointment.PENDING:
-            return { backgroundColor: colors.yellow[100], borderColor: colors.yellow[500] };
+            return {
+                backgroundColor: colors.yellow[100],
+                borderColor: colors.yellow[300],
+                text: 'Pendiente'
+            };
         case StatusAppointment.APPROVED:
-            return { backgroundColor: colors.green[100], borderColor: colors.green[500] };
+            return {
+                backgroundColor: colors.green[100],
+                borderColor: colors.green[500],
+                text: 'Aprobado'
+            };
         case StatusAppointment.REJECTED:
-            return { backgroundColor: colors.red[100], borderColor: colors.red[500] };
+            return {
+                backgroundColor: colors.red[100],
+                borderColor: colors.red[500],
+                text: 'Rechazado'
+            };
         default:
-            return { backgroundColor: colors.gray[100], borderColor: colors.gray[400] };
+            return {
+                backgroundColor: colors.gray[100],
+                borderColor: colors.gray[400],
+                text: 'Desconocido'
+            };
     }
 };
