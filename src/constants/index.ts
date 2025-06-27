4c55dd1f3a4898d8f62bb5a1fe30fe7c1542b5c4
Victor Aponte <vaponte520@gmail.com>
2025-06-27 19:20:31 -0300

    feat(constants): update API_BASE_URL for mac environment and comment out Windows configuration

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 //Para levantar en la windonws
-export const LOCAL_IP = '192.168.0.101';
-export const API_BASE_URL = `http://${LOCAL_IP}:3000`
+// export const LOCAL_IP = '192.168.0.101';
+// export const API_BASE_URL = `http://${LOCAL_IP}:3000`
 
 // //Para levantar en la mac
-// export const API_BASE_URL = 'http://10.0.2.2:3000';
+export const API_BASE_URL = 'http://192.168.22.121:3000';
