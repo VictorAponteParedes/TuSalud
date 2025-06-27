47837310ee0243e7eef47ea0fc4f17d68d8c30d9
Victor Aponte <vaponte520@gmail.com>
2025-06-27 19:20:01 -0300

    feat(userInfo): refactor useUserInformation to include patient details and improve data handling

--- src/hooks/useUserInfo.tsx
+++ src/hooks/useUserInfo.tsx
@@ -1,38 +1,49 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import AuthServices from "../services/auth";
+import {PatientDataType} from '../types/auth';
 
 
 const useUserInformation = () => {
-    const { user } = useAuth();
-    const authServices = new AuthServices();
+  const {user} = useAuth();
+  const authServices = new AuthServices();
 
-    const [profileInformation, setProfileInformation] = useState<string | null>(null);
-    const [loading, setLoading] = useState(true);
-    const fetchProfile = async () => {
-        try {
+  const [profileInformation, setProfileInformation] = useState<string | null>(
+    null,
+  );
+  const [patient, setPatient] = useState<PatientDataType | null>(null);
+  const [loading, setLoading] = useState(true);
 
-            const userInfo = await authServices.getUserInformation(user.token);
-            setProfileInformation(userInfo);
-
-        } catch (error) {
-            console.error('Error loading profile image:', error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-
-    useEffect(() => {
-
-        fetchProfile();
-    }, [user?.id]);
-
-    return {
-        profileInformation,
-        loading,
-
+  const fetchProfile = async () => {
+    try {
+      const userInfo = await authServices.getUserInformation(user.token);
+      setProfileInformation(userInfo.data);
+    } catch (error) {
+      console.error('Error loading profile image:', error);
+    } finally {
+      setLoading(false);
     }
+  };
+
+  const getPatientDetail = async () => {
+    try {
+      const patientDetail = await authServices.getPatientDetail(user.id);
+      setPatient(patientDetail);
+    } catch (error) {
+      console.error('Error fetching patients:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProfile();
+    getPatientDetail();
+  }, [user?.id]);
+
+  return {
+    profileInformation,
+    patient,
+    loading,
+  };
 }
 
 
