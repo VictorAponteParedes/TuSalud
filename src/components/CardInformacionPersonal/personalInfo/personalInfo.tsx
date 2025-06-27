5ea0e133f84aec7ea0bcd6155f2e00ac098cbbe8
Victor Aponte <vaponte520@gmail.com>
2025-06-27 19:19:52 -0300

    feat(personalInfo): refactor CardInformacionPersonal to use patient data and improve structure

--- src/components/CardInformacionPersonal/personalInfo/personalInfo.tsx
+++ src/components/CardInformacionPersonal/personalInfo/personalInfo.tsx
@@ -1,57 +1,67 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Calendar, Secure, Drop, Phone, Email } from '../../../helpers';
+import {View, Text} from 'react-native';
+import {Calendar, Secure, Drop, Phone, Email} from '../../../helpers';
 import SvgWrapper from '../../SvgWrapper';
 import colors from '../../../theme/colors';
-import { translate } from '../../../lang';
-import styles from './styles'
-import { CardInformacionPersonalProps } from '../../../types/modals';
+import {translate} from '../../../lang';
+import styles from './styles';
+import {PatientDataType} from '../../../types/auth';
 
+export type CardInformacionPersonalProps = {
+  patient: PatientDataType | null;
+};
 
+const CardInformacionPersonal = ({patient}: CardInformacionPersonalProps) => {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.title}>{translate('Information.title')}</Text>
 
+      <View style={styles.infoRow}>
+        <SvgWrapper color={colors.primary[400]} size={24}>
+          <Email />
+        </SvgWrapper>
+        <Text style={styles.infoText}>
+          {translate('Information.email')} {patient?.email}
+        </Text>
+      </View>
 
-const CardInformacionPersonal = (props: CardInformacionPersonalProps) => {
-    const { email, phone, BloodGroup, dateBirth, medicalSecure, style } = props;
-    return (
-        <View style={[styles.card, style]}>
-            <Text style={styles.title}>{translate('Information.title')}</Text>
+      <View style={styles.infoRow}>
+        <SvgWrapper color={colors.primary[400]} size={24}>
+          <Phone />
+        </SvgWrapper>
+        <Text style={styles.infoText}>
+          {translate('Information.phone')} {patient?.phone}
+        </Text>
+      </View>
 
-            <View style={styles.infoRow}>
-                <SvgWrapper color={colors.primary[400]} size={24}>
-                    <Email />
-                </SvgWrapper>
-                <Text style={styles.infoText}>{translate('Information.email')} {email}</Text>
-            </View>
+      <View style={styles.infoRow}>
+        <SvgWrapper color={colors.primary[400]} size={24}>
+          <Drop />
+        </SvgWrapper>
+        <Text style={styles.infoText}>
+          {translate('Information.bloodType')} {patient?.bloodType}
+        </Text>
+      </View>
 
-            <View style={styles.infoRow}>
-                <SvgWrapper color={colors.primary[400]} size={24}>
-                    <Phone />
-                </SvgWrapper>
-                <Text style={styles.infoText}>{translate('Information.phone')} {phone}</Text>
-            </View>
+      <View style={styles.infoRow}>
+        <SvgWrapper color={colors.primary[400]} size={24}>
+          <Calendar />
+        </SvgWrapper>
+        <Text style={styles.infoText}>
+          {translate('Information.dateBith')} {patient?.dateBirth}
+        </Text>
+      </View>
 
-            <View style={styles.infoRow}>
-                <SvgWrapper color={colors.primary[400]} size={24}>
-                    <Drop />
-                </SvgWrapper>
-                <Text style={styles.infoText}>{translate('Information.bloodType')} {BloodGroup}</Text>
-            </View>
-
-            <View style={styles.infoRow}>
-                <SvgWrapper color={colors.primary[400]} size={24}>
-                    <Calendar />
-                </SvgWrapper>
-                <Text style={styles.infoText}>{translate('Information.dateBith')} {dateBirth}</Text>
-            </View>
-
-            <View style={styles.infoRow}>
-                <SvgWrapper color={colors.primary[400]} size={24}>
-                    <Secure />
-                </SvgWrapper>
-                <Text style={styles.infoText}>{translate('Information.madicalSecure')} {medicalSecure}</Text>
-            </View>
-        </View>
-    );
+      <View style={styles.infoRow}>
+        <SvgWrapper color={colors.primary[400]} size={24}>
+          <Secure />
+        </SvgWrapper>
+        <Text style={styles.infoText}>
+          {translate('Information.madicalSecure')} {patient?.allergies}
+        </Text>
+      </View>
+    </View>
+  );
 };
 
 export default CardInformacionPersonal;
